537082d817f186b52473133658dc0f061d67da2e
bladeshape <tekigod@mail.ru>
2022-08-16 04:12:36 +0300

    ex213122

--- Ex10/Program.cs
+++ Ex10/Program.cs
@@ -12,4 +12,6 @@ Console.WriteLine("Введите трехзначное число: ");
         string input = Console.ReadLine();
         if(!input.Equals("exit"))
         Console.WriteLine("{0} -> {1}",input, input[1]);
+         else
+            break;
     }
